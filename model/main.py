8e141e04c0d099230486ed15d8ba4905e2751f9f
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-07-18 15:53:55 +0200

    fix .git/ references

--- model/main.py
+++ model/main.py
@@ -45,45 +45,16 @@ env = Environment(trajectory=label,
                   ncores=ncores,
                   use_pool=False, # likely not working w/ brian2
                   wrap_mode='QUEUE', # ??
-                  overwrite_file=False, # ??
-                  git_repository='./',
-                  git_fail = False)
+                  overwrite_file=False)
 
 
 tr = env.trajectory
 
-add_params(tr)
-tr.f_add_parameter('mconfig.git.sha1', str(commit))
-tr.f_add_parameter('mconfig.git.message', commit.message)
+if not args.testrun:
+    tr.f_add_parameter('mconfig.git.sha1', str(commit))
+    tr.f_add_parameter('mconfig.git.message', commit.message)
 
 tr.f_explore(explore_dict)
 
 env.run(run_model)
-
-
-if not first_run:
-
-    print("\nSimulation successful. Now merging...\n")
-
-    tr1 = Trajectory(name='tr1',
-                      add_time=False,
-                      filename=filename,
-                      dynamic_imports=[Brian2MonitorResult,
-                                       Brian2Parameter])
-
-    tr1.f_load(load_parameters=2, load_derived_parameters=2,
-               load_results=2)
-
-
-    tr2 = Trajectory(name='tr2',
-                      add_time=False,
-                      filename=filename,
-                      dynamic_imports=[Brian2MonitorResult,
-                                       Brian2Parameter])
-
-    tr2.f_load(load_parameters=2, load_derived_parameters=2,
-               load_results=2)
-
-    tr1.f_merge(tr2, backup_filename=True,
-                move_data=True, delete_other_trajectory = True)
 
